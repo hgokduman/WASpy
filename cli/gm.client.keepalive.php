992b709173f34e360b94d67119ed127c4433bab7
Halim Gökduman <halim@gokduman.nl>
2014-11-07 21:56:13 +0100

    initial setup for GearMan

---
+++ cli/gm.client.keepalive.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../config.inc.php');
+$client= new GearmanClient();
+$client->addServer();
+
+while(true) {
+	echo $client->do(WASPY_GMAN . '_PollMessages', 1);
+	echo $client->do(WASPY_GMAN . '_SendPong', 1);
+	sleep(5);
+}
+?>
---
+++ cli/gm.worker.connect.php
@@ -0,0 +1,38 @@
+<?php
+require_once('../config.inc.php');
+require_once(WASPY_DIR . '/libs/functions.inc.php');
+require_once(WASPY_DIR . '/libs/WhatsAPIPlus.class.php');
+require_once(WASPY_DIR . '/libs/WhatsAPIPlusEventListener.class.php');
+
+$GWorker= new GearmanWorker();
+$GWorker->addServer();
+$GWorker->setId(WASPY_GMAN . '_Connect_' . uniqid(true));
+
+$WhatsApp = new WhatsAPIPlus(PHONE_NUMBER, PHONE_IDENT, PHONE_ALIAS, PHONE_PASS, DEBUG);
+$WhatsApp->eventManager()->addEventListener(new WhatsAPIPlusEventListener());
+$WhatsApp->connectAndLogin();
+
+function GetWhatsApp() {
+    global $WhatsApp;
+    return $WhatsApp;
+}
+
+$GWorker->addFunction(WASPY_GMAN . '_RequestLastSeen', function(GearmanJob $job) {
+	$phone = $job->workload();
+	GetWhatsApp()->sendGetRequestLastSeen($phone);
+});
+$GWorker->addFunction(WASPY_GMAN . '_PresenceSubscribe', function(GearmanJob $job) {
+	GetWhatsApp()->sendPresenceSubscription($job->workload());
+});
+$GWorker->addFunction(WASPY_GMAN . '_PresenceUnsubscribe', function(GearmanJob $job) {
+	GetWhatsApp()->sendPresenceUnsubscription($job->workload());
+});
+$GWorker->addFunction(WASPY_GMAN . '_PollMessages', function(GearmanJob $job) {
+	while(GetWhatsApp()->pollMessage());
+});
+$GWorker->addFunction(WASPY_GMAN . '_SendPong', function(GearmanJob $job) {
+	GetWhatsApp()->sendPong(time());
+});
+
+while ($GWorker->work());
+?>
